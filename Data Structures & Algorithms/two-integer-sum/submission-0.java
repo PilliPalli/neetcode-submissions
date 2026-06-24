8ef78add52543d59a457f9040edfaeafa66ee0d6
PilliPalli <110674069+PilliPalli@users.noreply.github.com>
2026-06-24 17:03:36 +0200

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> valueToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            int complement = target - current;
+
+            if (valueToIndex.containsKey(complement)) {
+                return new int[] {valueToIndex.get(complement), i};
+            }
+
+            valueToIndex.put(current, i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution exists");
+    }
+}
